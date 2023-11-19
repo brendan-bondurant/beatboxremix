3613623890d7eae57bca6e147509c63b672f88e8
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-19 12:00:38 -0700

    Test:Feature test added for node:exists, has data, next node

---
+++ spec/node_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Node do
+  describe 'initialize' do
+    it 'exists' do
+      node = Node.new("plop")
+      expect(node).to be_instance_of(Node)
+    end
+    it 'has data' do
+      node = Node.new("plop")
+      expect(node.data).to eq("plop")
+    end
+    it 'has next node' do
+      node = Node.new("plop")
+      expect(node.next_node).to eq(nil)
+    end
+  end
+end
