120ac7a759c123d840bbb21c2cb049e5d3c9dcbf
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-19 12:33:13 -0700

    Feat:Count and empty method work and pass tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,6 +6,20 @@ class LinkedList
 
   def append(data)
     @head ||= Node.new(data) #with memoization
-    
+  end
+
+  def count
+    return 0 if empty?
+    count = 1
+    current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        count += 1
+      end
+    count
+  end
+
+  def empty?
+    head.nil? 
   end
 end
