925fa294b57c5e63a9de878a38e7b1fad63d3d79
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-19 12:40:57 -0700

    Test:Add testing to expand append method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -11,30 +11,43 @@ RSpec.describe LinkedList do
       list = LinkedList.new
       expect(list.head).to eq(nil)
     end
-    it 'can append' do
+  end
+  
+  describe 'methods' do
+    it 'can append one' do
       list = LinkedList.new
       list.append("doop")
       expect(list.head.data).to eq("doop")
       expect(list.head.next_node).to eq(nil)
     end
-
+    
+    it 'can append more than one' do
+      list = LinkedList.new
+      list.append("doop")
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+      list.append("deep")
+      expect(list.count).to eq(2)
+      expect(list.head.next_node).to eq("deep")
+    end
+    
     it 'can test if head is empty' do
       list = LinkedList.new
       expect(list.empty?).to eq(true)
       list.append("doop")
       expect(list.empty?).to eq(false)
     end
-
+    
     it 'can be counted' do
       list = LinkedList.new
       list.append("doop")
       expect(list.count).to eq(1)
     end
-
-    it 'can be turned to a string' do 
+    
+    it 'can be turned into a string' do 
       list = LinkedList.new
       list.append("doop")
-    expect(list.to_string).to eq("doop")
+      expect(list.to_string).to eq("doop")
     end
   end
 end
