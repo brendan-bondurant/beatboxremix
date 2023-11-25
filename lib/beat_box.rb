e20151918449d8b01f99650e2653de702c009680
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-24 20:42:16 -0700

    Feat:BeatBox append method written, rename later?

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -3,4 +3,12 @@ class BeatBox
   def initialize
     @list = LinkedList.new
   end
+
+  def append(data)
+    multiple_nodes = data.split
+      multiple_nodes.each do |single_node|
+      list.append(single_node)
+    end
+  list
+  end
 end
